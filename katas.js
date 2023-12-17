e066ac28ed372452dda5adec1c73b5f713204525
Miranda Muñoz <mirandamunllor@gmail.com>
2023-12-17 12:43:46 +0100

    Create katas.js
    a

---
+++ katas.js
@@ -0,0 +1,17 @@
+// Dado un array de numeros enteros encuentra aquel que aparece un numero de veces impar.
+    // partimos de un array y queremos que se nos devuelva un parámetro
+
+function findOdd(A) {
+if (A % 2 !== 0) {
+    return A;
+} return none;
+};
+
+// devuelve el numero de vocales que tiene un string (a,e,i,o,u)
+
+function getCount(s) {
+    return (str.filter)
+  };
+
+const vocalCount = function(str)
+// string.filter
